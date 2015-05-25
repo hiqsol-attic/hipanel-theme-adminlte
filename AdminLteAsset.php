56e7431e517693ed00ec816a9a753c2a593ef8cf
Andrii Vasyliev <sol@hiqdev.com>
2015-05-25 16:47:37 +0000

    fixed asset file

--- AdminLteAsset.php
+++ AdminLteAsset.php
@@ -1,31 +1,25 @@
 <?php
 /**
- * @link    http://hiqdev.com/hipanel-theme-adminlte2
- * @license http://hiqdev.com/hipanel-theme-adminlte2/license
- * @copyright Copyright (c) 2015 HiQDev
+ * @link        http://hiqdev.com/hipanel-theme-adminlte
+ * @license     BSD 3-clause
+ * @copyright   Copyright (c) 2015 HiQDev
  */
 
-namespace hipanel\themes\adminlte2;
+namespace hipanel\themes\adminlte;
 
 class AdminLteAsset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@bower/admin-lte';
     public $css = [
-        'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
-        'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
-        'dist/css/AdminLTE.css',
-        'dist/css/skins/_all-skins.css',
+        '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
+        '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
     ];
-    public $js = [
-        'plugins/slimScroll/jquery.slimscroll.min.js',
-        'plugins/fastclick/fastclick.min.js',
-        'dist/js/app.js',
-    ];
+
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
         'yii\bootstrap\BootstrapPluginAsset',
         'frontend\assets\AppAsset',
-        'hiqdev\assets\select2_bootstrap3_css\Asset',
+        'hiqdev\assets\adminlte\AdminLteAsset',
+        'hiqdev\assets\select2_bootstrap3_css\Select2Bootstrap3CssAsset',
     ];
 }
