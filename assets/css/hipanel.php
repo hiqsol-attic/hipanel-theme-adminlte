438a84b7d65f8732dae582d76781dfb2476dfae4
Andrii Vasyliev <sol@hiqdev.com>
2015-06-11 19:48:12 +0000

    minor

--- assets/css/hipanel.php
+++ assets/css/hipanel.php
@@ -9,10 +9,10 @@ $logo = Yii::$app->assetManager->getAssetUrl($asset, 'images/logo.png');
 ?>
 
 .logo-lg {
-    background: url(<?= $logo ?>) 5px 3px no-repeat;
+    background: url(<?= $logo ?>) 5px 2px no-repeat;
 }
 
 .logo-mini {
-    background: url(<?= $logo ?>) 7px 3px no-repeat;
+    background: url(<?= $logo ?>) 7px 2px no-repeat;
 }
 
