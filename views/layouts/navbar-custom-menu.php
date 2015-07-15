7cddbf0fba48934f1c9da059f0ed1413844d7e0a
Andrii Vasyliev <sol@hiqdev.com>
2015-07-15 11:20:38 +0000

    + views/layouts/user-menu.php

--- views/layouts/navbar-custom-menu.php
+++ views/layouts/navbar-custom-menu.php
@@ -1,6 +1,3 @@
-<?php
-use yii\helpers\Html;
-?>
 <ul class="nav navbar-nav">
     <!-- Messages: style can be found in dropdown.less-->
     <?php /*
@@ -90,36 +87,6 @@ use yii\helpers\Html;
             <?= $this->render('//layouts/gravatar', ['class' => 'user-image']); ?>
             <span class="hidden-xs"><?= \Yii::$app->user->identity->username; ?></span>
         </a>
-        <ul class="dropdown-menu">
-            <!-- User image -->
-            <li class="user-header">
-                <?= $this->render('//layouts/gravatar', ['size' => 90]); ?>
-                <p>
-                    <?= \Yii::$app->user->identity->username; ?> - <?= \Yii::$app->user->identity->type; ?>
-                    <small>Member since Nov. 2014</small>
-                </p>
-            </li>
-            <!-- Menu Body -->
-            <li class="user-body">
-                <div class="col-xs-4 text-center">
-                    <?= Html::a(\Yii::t('app', 'Theme Settings'), ['/thememanager/settings']); ?>
-                </div>
-                <div class="col-xs-4 text-center">
-                    <?= Html::a(\Yii::t('app', 'Recharge account'), []); ?>
-                </div>
-                <div class="col-xs-4 text-center">
-                    <?= Html::a(\Yii::t('app', 'Add New Ticket'), []); ?>
-                </div>
-            </li>
-            <!-- Menu Footer-->
-            <li class="user-footer">
-                <div class="pull-left">
-                    <?= Html::a(\Yii::t('app', 'Profile'), ['/logout'], ['class' => 'btn btn-default btn-flat']); ?>
-                </div>
-                <div class="pull-right">
-                    <?= Html::a(\Yii::t('app', 'Sign out'), ['/site/logout'], ['class' => 'btn btn-default btn-flat']); ?>
-                </div>
-            </li>
-        </ul>
+        <?= $this->render('//layouts/user-menu') ?>
     </li>
 </ul>
